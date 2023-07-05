cf0a8df67e7882c3d1f6b5220832a28fce6326b2
医升 <huaiweiyang.yhw@alibaba-inc.com>
2023-07-05 12:06:27 +0800

    添加单选框

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,81 @@
-// src/App.tsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+
+type OptionType = {
+  [key: string]: {
+    headerClass: string;
+    bodyClass: string;
+  };
+};
+
+const options: OptionType = {
+  其他入库: {
+    headerClass: "scm-ant-table-header",
+    bodyClass: "scm-ant-table-body",
+  },
+  其他出库: { headerClass: "c", bodyClass: "d" },
+  报损出库: {
+    headerClass: "cook-table-header.cook-table-sticky-holder",
+    bodyClass: "cook-table-body",
+  },
+};
 
 const App: React.FC = () => {
-  const [headerClass, setHeaderClass] = useState('scm-ant-table-header');
-  const [headerName, setHeaderName] = useState('标准数量');
-  const [bodyClass, setBodyClass] = useState('scm-ant-table-body');
-  const [inputValue, setInputValue] = useState('100');
+  const [option, setOption] = useState("报损出库");
+  const [headerClass, setHeaderClass] = useState(options[option].headerClass);
+  const [headerName, setHeaderName] = useState("标准数量");
+  const [bodyClass, setBodyClass] = useState(options[option].bodyClass);
+  const [inputValue, setInputValue] = useState("100");
+
+  useEffect(() => {
+    setHeaderClass(options[option].headerClass);
+    setBodyClass(options[option].bodyClass);
+  }, [option]);
 
   const fillTable = () => {
     chrome.runtime.sendMessage({
-      action: 'fill',
+      action: "fill",
       headerClass,
       headerName,
       bodyClass,
-      inputValue
+      inputValue,
     });
   };
 
   return (
     <div>
       <h1>Table Filler</h1>
+      <select value={option} onChange={(e) => setOption(e.target.value)}>
+        {Object.keys(options).map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
       <input
         type="text"
         placeholder="Header Class"
         value={headerClass}
-        onChange={e => setHeaderClass(e.target.value)}
+        onChange={(e) => setHeaderClass(e.target.value)}
       />
       <input
         type="text"
         placeholder="Header Name"
         value={headerName}
-        onChange={e => setHeaderName(e.target.value)}
+        onChange={(e) => setHeaderName(e.target.value)}
       />
       <input
         type="text"
         placeholder="Body Class"
         value={bodyClass}
-        onChange={e => setBodyClass(e.target.value)}
+        onChange={(e) => setBodyClass(e.target.value)}
       />
       <input
         type="text"
         placeholder="Input Value"
         value={inputValue}
-        onChange={e => setInputValue(e.target.value)}
+        onChange={(e) => setInputValue(e.target.value)}
       />
-      <button onClick={fillTable}>
-        Fill Table
-      </button>
+      <button onClick={fillTable}>Fill Table</button>
     </div>
   );
 };
